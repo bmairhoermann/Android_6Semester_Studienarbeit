73735f47b7b41903f9284aa383a4ba1f4d353ab9
philippwinterholler <philipp.winterholler@hof-university.de>
2016-06-27 16:55:25 +0200

    Update shareController

--- Studienarbeit_Android_SS16/app/src/main/java/de/hof_university/studienarbeitss16/studienarbeit_android_ss16/Controller/shareController.java
+++ Studienarbeit_Android_SS16/app/src/main/java/de/hof_university/studienarbeitss16/studienarbeit_android_ss16/Controller/shareController.java
@@ -34,20 +34,35 @@ public class shareController {
         this.mapsActivity = mapsActivity;
     }
 
-    public void check(){
+    public TrackModel check(){
         TrackModel trackModel = new TrackModel();
 
         trackModel.title = "MyFirstTrack";
         trackModel.firstPosition = new LatitudeLongitudeModel(50.32578791, 11.94006134, 0.0f, 1466951089000l);
         trackModel.lastPosition = new LatitudeLongitudeModel(50.32576226, 11.94002498, 0.0f, 1466951098000l);
-
+        /*
+        Firstposition: 50.32578791, 11.94006134, speed: 0.0, time 1466951089000
+        Lastposition: 50.32576226, 11.94002498, speed: 0.0, time 1466951098000
+        Listitem: 50.32578791, 11.94006134, speed: 0.0, time 1466951089000
+        Listitem: 50.3257799, 11.94005303, speed: 0.0, time 1466951090000
+        Listitem: 50.32577574, 11.9400563, speed: 1.031746, time 1466951091000
+        Listitem: 50.32576825, 11.94003625, speed: 0.0, time 1466951092000
+        Listitem: 50.32576173, 11.94001859, speed: 0.0, time 1466951094000
+        Listitem: 50.32576763, 11.94003366, speed: 0.0, time 1466951096000
+        Listitem: 50.32576226, 11.94002498, speed: 0.0, time 1466951098000
+        Listitem: 50.32576226, 11.94002498, speed: 0.0, time 1466951098000
+            */
         trackModel.trackList.add(new LatitudeLongitudeModel(50.32578791, 11.94006134, 0.0f, 1466951089000l));
         trackModel.trackList.add(new LatitudeLongitudeModel(50.32577574, 11.9400563, 1.031746f, 1466951091000l));
         trackModel.trackList.add(new LatitudeLongitudeModel(50.32576173, 11.94001859, 0.0f, 1466951094000l));
+        trackModel.trackList.add(new LatitudeLongitudeModel(50.3257799, 11.94005303, 1.02f, 1466951095000l));
+        return trackModel;
 
     }
 
     public void shareTrack(){
+        TrackModel myTrackModel = check();
+
 
         /*
         ShareOpenGraphObject object = new ShareOpenGraphObject.Builder()
@@ -79,8 +94,9 @@ public class shareController {
                 test.putString("og:title","Motorrad Tour");
             test.putString("og:description","Ist mit der GPS-Tracker App eine Motorrad Tour gefahren");
                 //Dauer
-        test.putInt("fitness:duration:value",10);
+        test.putLong("fitness:duration:value",myTrackModel.lastPosition.timeStamp - myTrackModel.firstPosition.timeStamp);
         test.putString("fitness:duration:units", "s");
+
                 //Entfernung
         test.putInt("fitness:distance:value",122);
         test.putString("fitness:distance:units","km");
@@ -89,9 +105,9 @@ public class shareController {
 
         test.putDouble("fitness:speed:value",10.21);
         test.putString("fitness:speed:units","m/s");
-        for(int i = 0; i <10 ; i++){
-            test.putDouble("fitness:metrics["+i+"]:location:latitude",i+10.1023912);
-            test.putDouble("fitness:metrics["+i+"]:location:longitude",(-i*2)+11.9905178);
+        for(int i = 0; i < myTrackModel.trackList.size();  i++){
+            test.putDouble("fitness:metrics["+i+"]:location:latitude",myTrackModel.trackList.get(i).latitude);
+            test.putDouble("fitness:metrics["+i+"]:location:longitude",myTrackModel.trackList.get(i).longitude);
         }
 
 
