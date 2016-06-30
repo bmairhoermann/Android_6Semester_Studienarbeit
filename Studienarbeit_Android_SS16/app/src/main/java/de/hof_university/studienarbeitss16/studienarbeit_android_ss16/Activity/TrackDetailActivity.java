476a9e20dee9e47cc3e72c5d96312af458f3b279
Brian Mairhörmann <brian.mairhoermann@hof-university.de>
2016-06-30 09:44:56 +0200

    TrackDetailActivity

---
+++ Studienarbeit_Android_SS16/app/src/main/java/de/hof_university/studienarbeitss16/studienarbeit_android_ss16/Activity/TrackDetailActivity.java
@@ -0,0 +1,15 @@
+package de.hof_university.studienarbeitss16.studienarbeit_android_ss16.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.hof_university.studienarbeitss16.studienarbeit_android_ss16.R;
+
+public class TrackDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_detail);
+    }
+}
